6e56d71f62fe6deafb575e14108f6622247a218e
houzkin <houzkin@gmail.com>
2016-12-21 13:02:26 +0900

    weakevent

---
+++ HouzkinLibrary/HouzkinArchitecture/Architecture/PropertyChainChangedWeakEventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Livet;
+
+namespace Houzkin.Architecture {
+	public class PropertyChainChangedWeakEventListener : WeakEventListener<PropertyChangedEventHandler,PropertyChangedEventArgs> {
+		public PropertyChainChangedWeakEventListener(Action<PropertyChangedEventHandler> add, Action<PropertyChangedEventHandler> remove, EventHandler<PropertyChangedEventArgs> handler)
+			: base(h => new PropertyChangedEventHandler(h), add, remove, handler) {
+
+		}
+	}
+}
